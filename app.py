47831c69d06d2d8b89b899a490436e00d5513a77
Eleni Mandana <elemandana@gmail.com>
2022-10-31 00:26:23 +0200

    redirect works

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, redirect
-from requests import request
+from unittest import result
+from flask import Flask, render_template, request, redirect, url_for
+ 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    global results
+    if request.method == 'POST':
+        results = request.form.get('query')  
+        return redirect('/result')
+        
     return render_template('index.html')
+
+# Result page
+@app.route('/result', methods=['GET', 'POST'])
+def queries():
+    data = results
+    return render_template('result.html', uquery = data)
 
 @app.errorhandler(404)
 def page_not_found(e):
