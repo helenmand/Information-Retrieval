e49508169565cc659a8e76cf7f82a8b952e10588
SpyrKarv <sp.karvouniaris@yahoo.gr>
2022-11-13 15:43:57 +0200

    Not ready don't touch

--- cos_similarity.py
+++ cos_similarity.py
@@ -2,6 +2,7 @@ import data_processing as dp
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from heapq import nsmallest
 
 def doc_doc_similarity(Docs):
     tfidf_vectorizer = TfidfVectorizer()
@@ -13,19 +14,29 @@ def doc_query_similarity(Docs, query, numOfDocs):
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform(Docs)
     tfidf_query = tfidf_vectorizer.fit_transform(query)
-    similarity_list = []
+    similarity_dict = []
+    id = 0
     for i in range (0, numOfDocs):
-        similarity_list.append(cosine_similarity(tfidf_query, tfidf_matrix[i,:]))
+        sim_value = cosine_similarity(tfidf_query, tfidf_matrix[i,:])
+        similarity_dict[id] = sim_value
+        heap = [(-value, key) for key, value in similarity_dict.items()]
+        id += 1
+    return nsmallest(5, heap)    
+            
+
 
 Data_temp = pd.read_csv('Greek_Parliament_Proceedings_1989_2020_DataSample.csv')  
 Data = Data_temp.loc[(Data_temp['political_party'] != 'βουλη') & (Data_temp['political_party'] != 'μετωπο ευρωπαικης ρεαλιστικης ανυπακοης (μερα25)')]
 Data.reset_index(drop=True, inplace=True)
-Docs = dp.process(Data['speech'])
+Data_list = Data['speech'].values.tolist()
+Docs = dp.process(Data_list)
+query = dp.process('Οι Αγρότες.!')
+print (query)
 if (Docs == 1):
     print('wtf')
 else:
-    matrix = doc_doc_similarity(Docs)
-    print (matrix)
+    similar = doc_query_similarity(Docs, query, len(Docs))
+    print (similar)
 #print(Data)
 #print(len(matrix[0]))    
 #print (Data['speech'][len(matrix[0])-2])
--- data_processing.py
+++ data_processing.py
@@ -2,13 +2,21 @@ from greek_stemmer import stemmer
 
 def punctuation_removal(Data):
     preprocessed_data = []
-    for line in Data:
-        preprocessed_line = line.replace(',',' ').replace('.',' ').replace('-',' ').replace('–',' ').\
-        replace(':', ' ').replace('«',' ').replace('»',' ').replace(';',' ').replace('!',' ').\
-        replace('\t',' ').replace('έ','ε').replace('ά','α').replace('ή','η').replace('ό','ο').\
-        replace('ύ','υ').replace('ί','ι').replace('ώ','ω').lower().split(' ')
-        preprocessed_line = [i for i in preprocessed_line if i] 
-        preprocessed_data.append(preprocessed_line)
+    if (type(Data) is list):
+        for line in Data:
+            preprocessed_line = line.replace(',',' ').replace('.',' ').replace('-',' ').replace('–',' ').\
+            replace(':', ' ').replace('«',' ').replace('»',' ').replace(';',' ').replace('!',' ').\
+            replace('\t',' ').replace('έ','ε').replace('ά','α').replace('ή','η').replace('ό','ο').\
+            replace('ύ','υ').replace('ί','ι').replace('ώ','ω').lower().split(' ')
+            preprocessed_line = [i for i in preprocessed_line if i] 
+            preprocessed_data.append(preprocessed_line)
+    else:
+        preprocessed_data = Data.replace(',', '').replace('.', '').replace('-', '').replace('–', '').\
+        replace(':', '').replace('«', '').replace('»', '').replace(';', '').replace('!', '').\
+        replace('\t', '').replace('έ','ε').replace('ά','α').replace('ή','η').replace('ό','ο').\
+        replace('ύ','υ').replace('ί','ι').replace('ώ','ω').lower()
+        preprocessed_data = [i for i in preprocessed_data if i]
+        preprocessed_data = ''.join(preprocessed_data)
     return preprocessed_data
 
 def stop_word_removal(preprocessed_data):
@@ -18,25 +26,53 @@ def stop_word_removal(preprocessed_data):
         for stopword in file.readlines():
             stopword = stopword[:-1]
             stop_words_array.append(stopword)
-    for line in preprocessed_data:
+    if (type(preprocessed_data) is list):        
+        for line in preprocessed_data:
+            for stopword in stop_words_array:
+                line = [word for word in line if word != stopword]
+            preprocessed_data1.append(line)
+    else:
+        #temp_list = preprocessed_data.split(' ')
+        #print(temp_list)
+        #print(preprocessed_data)
         for stopword in stop_words_array:
-            line = [word for word in line if word != stopword]
-        preprocessed_data1.append(line)
+            preprocessed_data1 = [word for word in preprocessed_data if word != stopword]
+        print(preprocessed_data1)    
+        preprocessed_data1 = ''.join(preprocessed_data)
+        #print(preprocessed_data1)                        
     return preprocessed_data1
 
 def stemming(preprocessed_data):
     preprocessed_data1 = []
-    for line in preprocessed_data:
+    if (type(preprocessed_data) is list):
+        for line in preprocessed_data:
+            index = 0
+            for word in line:
+                stemmed_word = stemmer.stem_word(word, 'VBG')
+                if stemmed_word.islower():
+                    del line[index]
+                else:
+                    line[index] = stemmed_word
+                index += 1
+            line1 = " ".join(line)   
+            preprocessed_data1.append(line1)
+    else:
         index = 0
-        for word in line:
-            stemmed_word = stemmer.stem_word(word, 'VBG')
+        if (" " in preprocessed_data):
+            for word in preprocessed_data:
+                stemmed_word = stemmer.stem_word(word, 'VBG')
+                if stemmed_word.islower():
+                    del preprocessed_data[index]
+                else:
+                    preprocessed_data[index] = stemmed_word
+                index += 1
+            preprocessed_data1 = " ".join(preprocessed_data)
+        else:
+            stemmed_word = stemmer.stem_word(preprocessed_data, 'VBG')
             if stemmed_word.islower():
-                del line[index]
+                preprocessed_data1 = None
             else:
-                line[index] = stemmed_word
-            index += 1
-        line1 = " ".join(line)   
-        preprocessed_data1.append(line1)
+                preprocessed_data1 = [stemmed_word]          
     return preprocessed_data1            
 
 def process(Data):
