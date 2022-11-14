719ff22dc300421bd60fd777c241c015875bd84b
Eleni Mandana <elemandana@gmail.com>
2022-11-14 11:11:19 +0200

    comments

--- initialize.py
+++ initialize.py
@@ -2,6 +2,7 @@ import pandas as pd
 import data_processing as dp
 import cos_similarity as cos_sim
 import warnings
+
 warnings.filterwarnings("ignore")
 
 def init():
@@ -43,13 +44,14 @@ def init():
 
     return Data, Docs, member_dict, party_dict, tags_dict 
 
+"""
 Data, Docs, member_dict, party_dict, tags_dict = init()
 
 query = dp.process('Τουρκία')
 if (type(query) is int):
     print ('Bad query')
 
-print(cos_sim.doc_query_similarity(Docs, query, len(Docs)))
+print(cos_sim.doc_query_similarity(Docs, query, len(Docs)))"""
 #print(member_dict)
 #print('--------------------------------------')
 #print(party_dict)
