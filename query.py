bd508d016218499689695c4af578e21b12f4d022
Eleni Mandana <elemandana@gmail.com>
2023-01-23 11:43:04 +0200

    fixed similarities arguments

--- query.py
+++ query.py
@@ -8,8 +8,8 @@ Returns the 5 most similar documents (sittings) to a query:
 3. political party of the speaker
 4. tags - most frequent words in the speech
 """
-def get_sittings(query, Data, Docs, index_dict, words_dict, tags_dict):
-    similarities = cs.doc_query_similarity(Docs, words_dict, query)
+def get_sittings(query, Data, index_dict, words_dict, tags_dict):
+    similarities = cs.doc_query_similarity(words_dict, query)
     
     sittings = []
     for sitting in similarities:
@@ -57,61 +57,8 @@ def get_sittings_by_party(party, Data, index_dict, tags_dict, party_dict, member
             sittings.append([sitting_id] + get_sitting_info(sitting_id, Data, index_dict, tags_dict)[0::4])
     return sittings
 
-
 """ 
-# dummy functions 
-import csv
-import random
-import string
-import initialize as it
-
-def clean_tags(text):
-    speech_sample = [random.choice(text.split(' ')) for _ in range(0,5)]
-    speech_sample = (' '.join(speech_sample)).translate(str.maketrans('', '', string.punctuation))
-    return speech_sample
-
-def get_random_data():
-    sittings = []
-    # 0:'member_name', 1:'sitting_date', 'parliamentary_period', 'parliamentary_session', 4:'parliamentary_sitting', 
-    # 5: 'political_party', 'government', 'member_region', 'roles', 'member_gender', 'speech
-    with open('Greek_Parliament_Proceedings_1989_2020_DataSample.csv','r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        header = next(reader)
-
-        for row in reader:
-            speech_sample = clean_tags(row[10])
-            sittings.append([row[4], row[0], row[5], speech_sample, random.randint(0,100)/100])
-    
-    sample = random.sample(sittings, 5)
-    for i in range(0,5):
-        sample[i].insert(0, i)
-
-    return sample
-
-def sittings_by_speaker(speaker):
-    sittings = []
-    with open('Greek_Parliament_Proceedings_1989_2020_DataSample.csv','r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        header = next(reader)
-
-        for row in reader:
-            if row[0].lower() == speaker:
-                speech_sample = clean_tags(row[10])
-                sittings.append([row[4], row[5], speech_sample])
-        return sittings
-
-def sittings_by_party(party):
-    sittings = []
-    with open('Greek_Parliament_Proceedings_1989_2020_DataSample.csv','r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        header = next(reader)
-
-        for row in reader:
-            if row[5].lower() == party:
-                speech_sample = clean_tags(row[10])
-                sittings.append([row[4], row[0], speech_sample])
-        return sittings
-
+# for testing
 if __name__ == "__main__":
     global Data, Docs, member_dict, party_dict, tags_dict
     Data, Docs, member_dict, party_dict, tags_dict = it.init()
